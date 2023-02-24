188b10c2c6d676aae009250882b00ef481dfd299
Teo Constant <teoconstant@mbp-de-teo.home>
2023-02-24 14:52:19 +0100

    mamamia

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 3,
-      staticDistDir: "./dist/settings.wtf",
+      staticDistDir: "./dist/ng-github-lighthouse-ci",
     },
     assert: {
       assertions: {
