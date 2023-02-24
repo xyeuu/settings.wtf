4d5a7883007a1124d0b28ada0967ecbc5faa8118
Teo Constant <teoconstant@mbp-de-teo.home>
2023-02-24 15:20:27 +0100

    mamamiaaa

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,14 @@
 module.exports = {
   ci: {
+    collect: {
+      numberOfRuns: 3,
+      staticDistDir: "./",
+    },
+    assert: {
+      assertions: {
+        "first-contentful-paint": ["warn", { maxNumericValue: 4000 }],
+      },
+    },
     upload: {
       target: "temporary-public-storage",
     },
